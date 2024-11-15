61db00351beabba563d76dcbd553daf188a49236
hib-code <hibarochdi1234@gmail.com>
2024-11-15 18:24:33 +0100

    create pages

--- offre/src/App.jsx
+++ offre/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/SignIn";
+import Signup from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+
 
 function App() {
-  return (
-    <div>App</div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/sign-in" element={<Signin/>}/>
+    <Route path="/sign-up" element={<Signup/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/profil" element={<Profile/>}/>
+  </Routes>
+  
+  </BrowserRouter>
+  
 }
 
 export default App
