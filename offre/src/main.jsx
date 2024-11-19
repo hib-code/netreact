5419f2a8d250e3e3154487abb10e555cc8bef21d
hib-code <hibarochdi1234@gmail.com>
2024-11-19 09:03:10 +0100

    add redux persist

--- offre/src/main.jsx
+++ offre/src/main.jsx
@@ -1,12 +1,19 @@
+import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { store } from './redux/store.js';
-import { Provider, provider } from 'react-redux';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store.js';
+
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-)
+  <StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </StrictMode>
+);
